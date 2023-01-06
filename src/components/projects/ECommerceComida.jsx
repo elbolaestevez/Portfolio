ca6e144e736c026228eb2dd7e37950ee471fe915
elbolaestevez <elbolaestevez@gmail.com>
2023-01-06 14:57:51 -0300

    datajson update

--- src/components/projects/ECommerceComida.jsx
+++ src/components/projects/ECommerceComida.jsx
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useContext } from "react";
+import { translatorwords } from "../../context/translator";
 import Home from "../../assets/projects/HomeE-Commerce.png";
 import Productos from "../../assets/projects/Productos-E-Commerce.png";
 import Carrito from "../../assets/projects/Carrito-E-commerce-png.png";
@@ -7,15 +8,15 @@ import Detalle from "../../assets/projects/DetalleE-Commerce.png";
 import Admin from "../../assets/projects/admin Productos E-commerce.png";
 import Pedidos from "../../assets/projects/PedidosEcommerce.png";
 const ECommerceComida = () => {
+  const { setC, setL, language, content } = useContext(translatorwords);
+
   return (
     <div className="w-full h-full pt-16 bg-[#0a192f]">
       <div className="mb-36 mt-20">
         <img className="w-2/5 mx-auto" src={Home} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Este el home principal de la página donde vemos un carrouse donde
-            van pasando las fotos de nuestros productos populares, como también
-            mas abajo podemos ver estos mismos productos.
+            {content.ehome}
           </p>
         </div>
       </div>
@@ -23,11 +24,7 @@ const ECommerceComida = () => {
         <img className="w-2/5 mx-auto" src={Productos} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Aca podemos ver estos productos donde estan divididos por categoria
-            que al clickear cualquier de ellas te lleva a esos productos. Uno
-            puede ver la foto del producto, con su precio y su descripcion. Al
-            clickear el producto te lleva a otra ruta con su detalle completo y
-            la opcion de agregarla al carrito.
+            {content.eproductos}
           </p>
         </div>
       </div>
@@ -35,8 +32,7 @@ const ECommerceComida = () => {
         <img className="w-2/5 mx-auto" src={Detalle} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Aca podemos ver el detalle del producto, y la opcion de agregarla al
-            carrito
+            {content.edetalle}
           </p>
         </div>
       </div>
@@ -44,13 +40,7 @@ const ECommerceComida = () => {
         <img className="w-2/5 mx-auto" src={Carrito} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Aca se encuentra el carrito donde el usuario decide si comprar o no.
-            El usuario puede modificar las cantidades desde el carrito sin la
-            necesidad de volver atras para agregarlos. EL tacho de basura
-            elimina el producto con todas sus cantidades.Y se encuentra las dos
-            opciones para realizar el pago. Una vez hecho el pedido el carrito
-            se borra para que si el usuario al dia siguiente quiere comprar no
-            ve estos productos en el carrito.
+            {content.ecarrito}
           </p>
         </div>
       </div>
@@ -59,11 +49,7 @@ const ECommerceComida = () => {
         <img className="w-2/5 mx-auto" src={Pedidos} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Aca podemos ver el historial de productos que hizo el usuario,
-            ninguno de estos datos esta harcodeado, el id del pedido tiene en
-            cuenta el ultimo pedido y se le suma uno para que quede correcto. Y
-            todos esos datos como la fecha estan sacadas de la base de datos
-            donde esta la tabla con esos registros.
+            {content.epedidos}
           </p>
         </div>
       </div>
@@ -71,10 +57,7 @@ const ECommerceComida = () => {
         <img className="w-2/5 mx-auto" src={Calificaiones} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Aca el usuario puede calificar los productos que no fueron
-            calificados. Luego ese puntaje se le suma al promedio del puntaje de
-            ese producto. Tambien se le agrega un comentario para que quede
-            registrado las opiniones del usuario.
+            {content.ecalificaciones}
           </p>
         </div>
       </div>
@@ -83,11 +66,7 @@ const ECommerceComida = () => {
         <img className="w-2/5 mx-auto" src={Admin} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            El administrador puede agregar, editar y borrar productos, como se
-            puede ver aca al tocar el + que esta al lado de Carne, que solo se
-            habilitado cuando el administador se loguea. No esta aca en la foto,
-            pero el administrador puede hacer administrador a los usuarios y a
-            su vez sacarle la administracion
+            {content.eadmin}
           </p>
         </div>
       </div>
--- src/components/projects/MovieTmdb.jsx
+++ src/components/projects/MovieTmdb.jsx
@@ -1,22 +1,20 @@
-import React from "react";
+import React, { useContext } from "react";
+import { translatorwords } from "../../context/translator";
 import DetalleMovie from "../../assets/projects/DetalleMovie.png";
 import HomeMovie from "../../assets/projects/homeMovie.png";
 import Registro from "../../assets/projects/registroMovie.png";
 import Favoritos from "../../assets/projects/favoritosMovie.png";
 import Movietmbd2 from "../../assets/projects/Movietmdb.png";
 const MovieTmdb = () => {
+  const { setC, setL, language, content } = useContext(translatorwords);
+
   return (
     <div className="w-full h-full pt-16 bg-[#0a192f] mb-20">
       <div className="mb-36 mt-20">
         <img className="w-2/5 mx-auto" src={HomeMovie} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Esta es la pantalla de inicio, donde vemos un Carrousel con las
-            mejores peliculas que van pasando automaticamente. Tenemos un
-            navegador, donde nos lleva al Registro de la pagina, a los
-            favoritos, a los usuarios registrados y al Home. Tambien tenemos dos
-            inputs, una para peliculas y otra para series donde al colocar un
-            nombre nos tira el cartel abajo con todas sus peliculas o series.
+            {content.mhome}
           </p>
         </div>
       </div>
@@ -24,11 +22,7 @@ const MovieTmdb = () => {
         <img className="w-2/5 mx-auto" src={DetalleMovie} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Aca podemos ver el detalle de una pelicula que quiero seleccionar
-            con su puntuacion, su titulo, y un detalle. Ademas también tenemos
-            un botón viewMore donde al clickear se abre un texto con la critica
-            de la pelicula que fue sacado a través de la libreria Puppeteer con
-            Web Scraping.
+            {content.mdetalle}
           </p>
         </div>
       </div>
@@ -37,9 +31,7 @@ const MovieTmdb = () => {
         <img className="w-2/5 mx-auto" src={Favoritos} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Aca podemos ver las peliculas favoritas de cada usuario, donde uno
-            puede removerlas sin actualizar la pagina. Ademas al hacer click en
-            el nombre te lleva al detalle de la pelicula
+            {content.mfavoritos}
           </p>
         </div>
       </div>
@@ -47,9 +39,7 @@ const MovieTmdb = () => {
         <img className="w-2/5 mx-auto" src={Registro} alt="HTML icon" />
         <div className=" w-2/4  mx-auto ">
           <p className="my-4 text-4xl text-white text-justify font-style: italic">
-            Aca el usuario de puede Loguiar a la pagina con su email y la
-            contraseña. Simple, fue uno de mis primeros proyectos y al tocar
-            Registrarse te lleva a otra ruta dónde podemos crear un usuario.
+            {content.mregistro}
           </p>
         </div>
       </div>
