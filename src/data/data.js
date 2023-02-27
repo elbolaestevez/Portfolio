afea34032f5d5d135673b2c47198f5355b0841c5
elbolaestevez <elbolaestevez@gmail.com>
2023-02-27 20:56:47 -0300

    update png

--- src/data/data.js
+++ src/data/data.js
@@ -1,7 +1,7 @@
 import WorkIm from "../assets/projects/Movietmdb.png";
 import RealEst from "../assets/projects/ComoenCasa.png";
 import Itesa from "../assets/projects/itesahome.png";
-import Tinder from "../assets/projects/tinder.png";
+import Tinder from "../assets/projects/Tinder.png";
 
 export const data = [
   {
